1d07ddb0b6b400e5cc281a3f0c691e76bf4b2187
nizam1643 <nizam4din@gmail.com>
2025-11-03 13:47:27 +0800

    feat(routes): add authentication routes for registration, login, password reset, and email verification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+Route::middleware('auth')->group(function () {
+    Route::get('/upload-index', [UploadController::class, 'index'])->name('upload.index');
+    Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
+    Route::get('/uploads', [UploadController::class, 'list'])->name('uploads.list');
 });
